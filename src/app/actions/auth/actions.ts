df333123a5d3c102efa69a242241465036fda7ad
Thiago Rodrigues <thiagothogs@icloud.com>
2024-06-26 06:05:58 -0300

    feat: added request hooks and providers

---
+++ src/app/actions/auth/actions.ts
@@ -0,0 +1,22 @@
+'use server'
+import { signIn, signOut } from "@/auth";
+
+export async function onSubmitGitHubSignIn() {
+  await signIn('github', {
+    redirectTo: '/dashboard'
+  });
+}
+
+export async function onSubmitCredentialsSignIn(email: string, password: string) {
+  await signIn('credentials', {
+    email,
+    password,
+    redirectTo: '/dashboard'
+  });
+}
+
+export async function onSubmitSignOut() {
+  await signOut({
+    redirectTo: '/auth/signin'
+  });
+}
---
+++ src/app/hooks/useBaseRequest.tsx
@@ -0,0 +1,31 @@
+'use client';
+import axios, { AxiosInstance } from "axios";
+import { useSession } from "next-auth/react";
+
+export function useBaseRequest() {
+  const session = useSession();
+
+  const createAxiosInstance = (): AxiosInstance => {
+    const instance = axios.create({
+      baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: '*/*',
+        Authorization: `Bearer ${session?.data?.user?.accessToken}`,
+      }
+    })
+    instance.interceptors.response.use(async response => {
+      try {
+        return response
+      } catch (error) {
+        console.error(error)
+        return Promise.reject(error)
+      }
+    })
+    return instance;
+  }
+
+  return {
+    instance: createAxiosInstance(),
+  }
+}
